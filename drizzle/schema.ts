f3e4c2c90113c41433664677ea491db6cc5fe5e3
Isaiah Hunter <hunter.isaiah96@gmail.com>
2023-10-28 21:46:35 -0400

    schema updates

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,4 +1,4 @@
-import { int, text, varchar, mysqlTable } from "drizzle-orm/mysql-core"
+import { int, text, varchar, mysqlEnum, mysqlTable } from "drizzle-orm/mysql-core"
 import { relations } from "drizzle-orm"
 
 export const users = mysqlTable("users", {
@@ -8,20 +8,38 @@ export const users = mysqlTable("users", {
   password: text("password").notNull(),
 })
 
-export const usersRelations = relations(users, ({ many }) => ({
-  blocks: many(blocks),
-}))
+export const profile = mysqlTable("profile", {
+  id: int("id").primaryKey().autoincrement().notNull(),
+  userId: int("user_id").notNull(),
+  displayName: varchar("display_name", { length: 255 }).unique().notNull(),
+  bio: text("bio"),
+})
 
 export const blocks = mysqlTable("blocks", {
   id: int("id").primaryKey().autoincrement().notNull(),
-  userId: int("user_id"),
+  userId: int("user_id").notNull(),
   name: text("name").notNull(),
   link: text("link"),
+  block_type: mysqlEnum("block_type", ["link", "header"]).notNull(),
 })
 
-export const blocksRelations = relations(blocks, ({ one }) => ({
+export const profileUserRelation = relations(profile, ({ one }) => ({
   user: one(users, {
-    fields: [blocks.userId],
+    fields: [profile.userId],
     references: [users.id],
   }),
 }))
+
+export const blockToProfileRelations = relations(blocks, ({ one }) => ({
+  user: one(profile, {
+    fields: [blocks.userId],
+    references: [profile.id],
+  }),
+}))
+
+// export const blockToUserRelations = relations(blocks, ({ one }) => ({
+//   user: one(users, {
+//     fields: [blocks.userId],
+//     references: [users.id],
+//   }),
+// }))
--- server/planetscale-service.ts
+++ server/planetscale-service.ts
@@ -1,8 +1,9 @@
 import { drizzle } from "drizzle-orm/planetscale-serverless"
 import { connect } from "@planetscale/database"
+import * as schema from "@/drizzle/schema"
 // create database connection
 const connection = connect({
   url: process.env.DATABASE_URL,
 })
 
-export const db = drizzle(connection)
+export const db = drizzle(connection, { schema })
