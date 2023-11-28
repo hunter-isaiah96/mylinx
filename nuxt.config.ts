dd48af9921929eb0df3a782ff90ac24c6a08da06
Isaiah Hunter <hunter.isaiah96@gmail.com>
2023-11-28 02:18:18 -0500

    nuxt auth config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,6 @@ export default defineNuxtConfig({
       enableRefreshOnWindowFocus: false,
       enableRefreshPeriodically: false,
     },
-    isEnabled: true,
     globalAppMiddleware: true,
     provider: {
       type: "authjs",
